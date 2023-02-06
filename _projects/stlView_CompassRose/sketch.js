8ae7b8734731e22a7bdab159e00a472beba36fa4
touchtt <jesse@the-touch.com>
2023-02-06 00:22:19 -0500

    changed slider ppsition

--- _projects/stlView_CompassRose/sketch.js
+++ _projects/stlView_CompassRose/sketch.js
@@ -42,11 +42,11 @@ function setup() {
 
     povSlider = createSlider(0.0, 2.0, 0.0, 0.1);
 
-    povSlider.position(575, 225);
+    povSlider.position(475, 225);
 
     frameXrot = createSlider(0.0, 360.0, 0.0, 1);
 
-    frameXrot.position(575, 265);
+    frameXrot.position(475, 265);
 }
 
 function draw() {
